7ffd094c6e54f057c2f2acac441c6868712aa283
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-09-11 13:50:56 +0400

    v1.0 function to create an avatar for the user

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -59,6 +59,43 @@ class AuthController
             return false;
         }
     }
+    private function generateAvatarImage($text, $backgroundColor,string $firstname,string $lastname, string $username)
+    {
+        $canvas = imagecreatetruecolor($canvasWidth, $canvasHeight);
+
+        // Convertir la couleur d'arrière-plan en composantes RGB
+        $backgroundR = hexdec(substr($backgroundColor, 1, 2));
+        $backgroundG = hexdec(substr($backgroundColor, 3, 2));
+        $backgroundB = hexdec(substr($backgroundColor, 5, 2));
+
+        // Remplir le canvas avec la couleur d'arrière-plan
+        $backgroundColor = imagecolorallocate($canvas, $backgroundR, $backgroundG, $backgroundB);
+        imagefill($canvas, 0, 0, $backgroundColor);
+
+        // Définir la couleur du texte
+        $foregroundColor = imagecolorallocate($canvas, 255, 255, 255); // Blanc
+
+        // Centrer le texte dans le canvas
+        $fontSize = floor(100.00);
+        $fontPath = 'public/font/Rajdhani-SemiBold.ttf'; // Chemin vers le dossier des polices de caractères
+        $textBoundingBox = imageftbbox($fontSize, 0, $fontPath, $text);
+        $textWidth = $textBoundingBox[2] - $textBoundingBox[0];
+        $textHeight = $textBoundingBox[1] - $textBoundingBox[7];
+        $textX = ($canvasWidth - $textWidth) / 2;
+        $textY = ($canvasHeight - $textHeight) / 2 + $textHeight;
+
+        // Dessiner le texte sur le canvas avec la police de caractères par défaut
+        imagefttext($canvas, $fontSize, 0, $textX, $textY, $foregroundColor, $fontPath, $text);
+
+        // Enregistrer l'image dans un fichier PNG
+        $randomString = bin2hex(random_bytes(3)); // Génère une chaîne hexadécimale de 6 caractères
+        $avatarName = $randomString . '-' . $username.'.png';
+        $filename = 'public/images/avatars/' . $avatarName; // Chemin vers le dossier et nom du fichier d'avatar
+        imagepng($canvas, $filename);
+        imagedestroy($canvas);
+
+        return $avatarName;
+    }
     public function register(): void
     {
         $username = $this->verifyField('username');
