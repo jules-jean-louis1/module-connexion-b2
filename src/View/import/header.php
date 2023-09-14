4d12a57100fe866641269c4ca90db759389b3e14
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-09-14 21:12:40 +0200

    add link in header

--- src/View/import/header.php
+++ src/View/import/header.php
@@ -16,13 +16,35 @@
         <div>
             <ul class="flex items-center space-x-6">
                 <li>
-                    <a href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/moduleconnexionb2/docs'; ?>">Docs</a>
+                    <a href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/moduleconnexionb2/docs'; ?>" class="font-semibold hover:text-[#7a1ea1] transition duration-100">
+                        Docs
+                    </a>
                 </li>
                 <li>
-                    <a href="">Contact</a>
+                    <a href="https://jules-jean-louis.students-laplateforme.io/" class="flex items-center font-semibold hover:text-[#7a1ea1] transition duration-100">
+                        <span>Contact</span>
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"/>
+                                <path d="M11 13l9 -9"/>
+                                <path d="M15 4h5v5"/>
+                            </svg>
+                        </span>
+                    </a>
                 </li>
                 <li>
-                    <a href="">Projets</a>
+                    <a href="https://jules-jean-louis.students-laplateforme.io/Projet/index.php" class="flex items-center font-semibold hover:text-[#7a1ea1] transition duration-100">
+                        <span>Projets</span>
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"/>
+                                <path d="M11 13l9 -9"/>
+                                <path d="M15 4h5v5"/>
+                            </svg>
+                        </span>
+                    </a>
                 </li>
             </ul>
         </div>
