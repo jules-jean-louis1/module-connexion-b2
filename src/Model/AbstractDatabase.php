65ce315afbbbd662f1effcd830501ac35a7687e0
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-09-15 14:16:18 +0200

    add login to database

--- src/Model/AbstractDatabase.php
+++ src/Model/AbstractDatabase.php
@@ -18,6 +18,14 @@ abstract class AbstractDatabase implements DatabaseInterface
             echo 'Connexion échouée : ' . $e->getMessage();
             exit;
         }
+        // Connexion a la base de données ONLINE
+        /*try {
+            $this->bdd = new PDO('mysql:host=localhost;dbname=jules-jean_louis_moduleconnexionb2;charset=utf8', 'modulecob2', 'tMq&0s333');
+            $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'Connexion échouée : ' . $e->getMessage();
+            exit;
+        }*/
     }
     public function getBdd(): PDO
     {
