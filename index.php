20102448b40ed7aef25d09da761491d752cf2ea6
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-09-15 11:27:26 +0200

    404 page

--- index.php
+++ index.php
@@ -17,12 +17,12 @@ $router->setBasePath('/moduleconnexionb2');
 
 // map homepage
 $router->map( 'GET', '/', function() {
-    require 'src/View/home.php';
+    require_once 'src/View/home.php';
 });
 
 // map user details page
 $router->map( 'GET', '/profil/[i:id]', function( $id ) {
-    require 'src/View/profil.php';
+    require_once 'src/View/profil.php';
 });
 $router->map('GET', '/profil/[i:id]/info', function ($id) use ($userController) {
     $userController->getUserInfo($id);
@@ -31,12 +31,12 @@ $router->map('POST', '/profil/[i:id]/edit', function ($id) use ($userController)
     $userController->editUserInfo($id);
 });
 $router->map( 'GET', '/contact', function() {
-    require 'src/View/contact.php';
+    require_once 'src/View/contact.php';
 });
 
 // map admin page
 $router->map( 'GET', '/admin', function() {
-    require 'src/View/admin.php';
+    require_once 'src/View/admin.php';
 });
 $router->map('GET', '/admin/users/[*:search]?/[a:username]/[a:firstname]/[a:lastname]/[a:role]/[a:createdAt]/[a:updatedAt]', function ($search,$username, $firstname, $lastname, $role, $createdAt, $updatedAt) use ($adminController) {
     $adminController->getAllUsers($search, $username, $firstname, $lastname, $role, $createdAt, $updatedAt);
@@ -52,14 +52,14 @@ $router->map('POST', '/admin/users/role/[i:id]', function ($id) use ($adminContr
 
 //map docs page
 $router->map( 'GET', '/docs', function() {
-    require 'src/View/docs.php';
+    require_once 'src/View/docs.php';
 });
 /* Form Auth */
 $router->map('GET', '/login', function () {
-    require 'src/View/import/form/loginForm.php';
+    require_once 'src/View/import/form/loginForm.php';
 });
 $router->map('GET', '/register', function () {
-    require 'src/View/import/form/registerForm.php';
+    require_once 'src/View/import/form/registerForm.php';
 });
 $router->map('POST', '/register/submit', function () use ($authController) {
     $authController->register();
@@ -79,6 +79,6 @@ $match = $router->match();
 if( is_array($match) && is_callable( $match['target'] ) ) {
     call_user_func_array( $match['target'], $match['params'] );
 } else {
-    // no route was matched
+    require_once 'src/View/404.php';
     header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
 }
---
+++ src/View/404.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script defer type="module" src="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/moduleconnexionb2/public/js/home.js';?>"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/moduleconnexionb2/public/css/home.css';?>">
+    <title>Module.co</title>
+</head>
+<body>
+<header>
+    <?php require_once 'src/View/import/header.php'; ?>
+</header>
+<main class="h-[92vh]">
+    <div id="containerFormLoginRegister"></div>
+    <div id="dialogModal_Overlay"></div>
+    <section id="mainContainerHome" class="flex justify-between h-full">
+        <div id="left90degresponsive" class="flex items-center">
+            <h3 class="text-4xl uppercase">Erreur 404</h3>
+            <div class="w-10 bg-gray-600" id="barre_dialog"></div>
+        </div>
+        <div class="flex items-end" id="title_Homepage">
+            <div id="bottomPageTitle" class="flex flex-col items-end playfair_display">
+                <div class="font-bold uppercase text-[9rem] lg:-mt-6 lg:-mb-9">404</div>
+                <div class="font-bold uppercase text-[9rem] lg:-mt-6 lg:-mb-9">Erreur</div>
+            </div>
+        </div>
+    </section>
+</main>
+<footer>
+    <?php require_once 'src/View/import/footer.php'; ?>
+</footer>
+</body>
+</html>
