d16abf3a618dd7162b81d043b29829ab7d6f98bd
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2023-09-15 11:16:07 +0200

    fix title

--- src/View/home.php
+++ src/View/home.php
@@ -8,7 +8,7 @@
     <script defer type="module" src="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/moduleconnexionb2/public/js/home.js';?>"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="<?php echo 'http://' . $_SERVER['HTTP_HOST'] . '/moduleconnexionb2/public/css/home.css';?>">
-    <title>Home</title>
+    <title>Module.co</title>
 </head>
 <body>
     <header>
